2c3f71e86943950c9d53720344e20c0e565b1bb9
Mohit Gupta <mohitguptatech18@gmail.com>
2022-01-26 18:19:31 +0530

    header component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+      {/* Home */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAN4k4_b-AbwuNkm9jzUhtMRvAwbNrwEJM",
+  authDomain: "clone-d2539.firebaseapp.com",
+  projectId: "clone-d2539",
+  storageBucket: "clone-d2539.appspot.com",
+  messagingSenderId: "787997225756",
+  appId: "1:787997225756:web:18f7c68a12fe0f01bfc0d9",
+  measurementId: "G-20M3HM8WE0",
+};
