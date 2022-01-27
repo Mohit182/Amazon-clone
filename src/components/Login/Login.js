1b24d0c6a67e623525482d18a6198a6cd0df44c8
Mohit Gupta <mohitguptatech18@gmail.com>
2022-01-27 19:13:33 +0530

    login user functionality added through firebse

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,36 @@
 import React, { useState } from "react";
 import "./Login.css";
 import amazonLogo from "../../Assets/amazonLogo.png";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "../../firebase";
 
 function Login() {
+  const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const signInHandler = (e) => {
     e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
   };
   const registerHandler = (e) => {
     e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        console.log(auth);
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
   };
 
   return (
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAN4k4_b-AbwuNkm9jzUhtMRvAwbNrwEJM",
   authDomain: "clone-d2539.firebaseapp.com",
@@ -7,3 +11,10 @@ const firebaseConfig = {
   appId: "1:787997225756:web:18f7c68a12fe0f01bfc0d9",
   measurementId: "G-20M3HM8WE0",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
